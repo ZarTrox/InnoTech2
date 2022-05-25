d660401a8705f712db572dbadc9f352eeb472795
ZarTrox <buchner.benjamin99@web.de>
2022-05-25 18:47:39 +0200

    test new url

--- src/js/test.js
+++ src/js/test.js
@@ -3,3 +3,13 @@ function requestData(link) {
         .then(response => response.json())
         .then(data => console.log(data));
 }
+
+function requestData2(link) {
+    fetch(link)
+    .then(response => response.json())
+    .then(data => {
+        for(elment in data){
+            console.log(data[elment]["datum"])
+        }
+    });
+}
