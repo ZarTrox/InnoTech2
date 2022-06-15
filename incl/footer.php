2736bc2efb432e7033f01439ba7876ef009e888c
InnovationBuddy <uf6qr4w0r@mozmail.com>
2022-06-15 22:58:06 +0200

    2022-06-15 Manu V3

--- incl/footer.php
+++ incl/footer.php
@@ -1,3 +1,9 @@
 <div class="footer_container">
-
+    <div>
+        <p class="text-center pt-3 pb-3 m-0">
+            Copyright &copy; <?php echo date("Y"); ?><br>
+            GOOO Team<br>
+            Representiert durch: Benjamin Buchner, Marin Hornauer, Manuel Sauer, Tim-Julian Schwehn, Celina Söllner, Agnes Tremmel, Hai-Anh Vu, Selina Wölker
+        </p>
+    </div>
 </div>
