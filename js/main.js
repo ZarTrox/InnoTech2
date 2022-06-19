310849579bcaaedcfd2a935aa9afaa957bb99090
ZarTrox <buchner.benjamin99@web.de>
2022-06-19 21:49:59 +0200

    transitions and variable colors

--- js/main.js
+++ js/main.js
@@ -58,96 +58,137 @@ async function updateSvgWithData(data) {
         //Check time here
         await sleep(1000)
         if (data[element]) {
-            createSvgOrig(data[element]["werte"], data[element]["datum"]);
+            updateStationen(data[element]["werte"], data[element]["datum"]);
         } else {
             console.log("Element is empty!")
         }
     }
 }
 
-function createSvgOrig(dataArray, Zeit) {
+function createSvgUebersicht(dataArray, Zeit) {
+    /*
+        Selber erstellen oder bild hernehmen?
+    */
+}
+
+function createSvgHochregallager(dataArray, Zeit) {
     d3.xml("./media/img/Hochregallager.svg",
         function (error, documentFragment) {
             if (error) { console.log(error); return; }
 
             var svgNode = documentFragment
                 .getElementsByTagName("svg")[0];
-            var main_chart_svg = d3.select("#svgcontainer")
-            // if (document.getElementById("div"))
-            if (document.getElementById('svgcontainer').getElementsByTagName('svg').length == 0) {
+            var main_chart_svg = d3.select("#Hochregallager")
+            if (document.getElementById('Hochregallager').getElementsByTagName('svg').length == 0) {
                 main_chart_svg.node().appendChild(svgNode);
             }
             svg = main_chart_svg.select("svg")
             if (dataArray != "") {
-                updateStationen(svg, dataArray);
+                var colorReferenztasterVertikal = "red";
+                if (!dataArray[ArrayWithVariablesForHochregalLager[1]] == " true"){
+                    colorReferenztasterVertikal = "green";
+                }
+                var colorLichtschrankeInnen = "red";
+                if (!dataArray[ArrayWithVariablesForHochregalLager[0]] == " true"){
+                    colorLichtschrankeInnen = "green";
+                }
+                svg.selectAll("rect.Hochregallager").remove();
+                //Objekten wie dem turm oder dem motor eine id zuweise und diese dann bewegen
+                //hannah im Meeting zum motorhäuschen fragen
+                svg.append("rect")//H-vert&hori
+                    .attr("class", "Hochregallager")
+                    .attr("id", "ausrichtung")
+                    .attr("x", dataArray[ArrayWithVariablesForHochregalLager[5]])
+                    .attr("y", dataArray[ArrayWithVariablesForHochregalLager[4]])
+                    .attr("width", 100)
+                    .attr("height", 100)
+                    .attr('stroke', 'red')
+                    .attr("fill", "#ddd")
+                //Vergleiche ob sich etwas zum vorherigen verändert hat (Fkt aus alter Datei nehmen)
+                //Bzw den vergleich nicht hier sondern in einer funktion und nunr wahr oder falsch uebermitteln
+                if ((dataArray[ArrayWithVariablesForHochregalLager[5]] != 1) || (dataArray[ArrayWithVariablesForHochregalLager[4]] != 2)){
+                    svg.select("#ausrichtung")
+                    .transition()
+                    .duration(500)
+                    .attr("fill", "red")
+                }
+                
+                svg.append("circle")//Lichtschranke Innen
+                    .attr("class", "Hochregallager")
+                    .attr("cx", 290)
+                    .attr("cy", 420)
+                    .attr("r", 15)
+                    .attr('stroke', colorLichtschrankeInnen)
+                    .attr("fill", colorLichtschrankeInnen)
+                svg.append("circle")//Referenztaster vertikal
+                    .attr("class", "Hochregallager")
+                    .attr("cx", 240)
+                    .attr("cy", 130)
+                    .attr("r", 15)
+                    .attr('stroke', colorReferenztasterVertikal)
+                    .attr("fill", colorReferenztasterVertikal)
+                svg.append("circle") //Referenztaster Ausleger vorne
+                    .attr("class", "Hochregallager")
+                    .attr("cx", 200)
+                    .attr("cy", 420)
+                    .attr("r", 15)
+                    .attr('stroke', 'black')
+                    .attr("fill", "#000")
+                svg.append("rect") //Motorförderband
+                    .attr("class", "Hochregallager")
+                    .attr("x", 240)
+                    .attr("y", 500)
+                    .attr("width", 50)
+                    .attr("height", 50)
+                    .attr('stroke', 'red')
+                    .attr("fill", "#ddd")
             }
 
         });
 }
 
-function updateHochregallager(svg, dataArray) {
-    svg.selectAll("rect.Hochregallager").remove();
-    //Objekten wie dem turm oder dem motor eine id zuweise und diese dann bewegen
-    //hannah im Meeting zum motorhäuschen fragen
-    svg.append("rect")//H-vert&hori
-        .attr("class", "Hochregallager")
-        .attr("x", dataArray[ArrayWithVariablesForHochregalLager[5]])
-        .attr("y", dataArray[ArrayWithVariablesForHochregalLager[4]])
-        .attr("width", 100)
-        .attr("height", 100)
-        .attr('stroke', 'red')
-        .attr("fill", "#ddd")
-    svg.append("circle")//Lichtschranke Innen
-        .attr("class", "Hochregallager")
-        .attr("cx", 290)
-        .attr("cy", 420)
-        .attr("r", 15)
-        .attr('stroke', 'black')
-        .attr("fill", "#000")
-    svg.append("circle")//Referenztaster vertikal
-        .attr("class", "Hochregallager")
-        .attr("cx", 240)
-        .attr("cy", 130)
-        .attr("r", 15)
-        .attr('stroke', 'black')
-        .attr("fill", "#000")
-    svg.append("circle") //Referenztaster Ausleger vorne
-        .attr("class", "Hochregallager")
-        .attr("cx", 200)
-        .attr("cy", 420)
-        .attr("r", 15)
-        .attr('stroke', 'black')
-        .attr("fill", "#000")
-    svg.append("rect") //Motorförderband
-        .attr("class", "Hochregallager")
-        .attr("x", 240)
-        .attr("y", 500)
-        .attr("width", 50)
-        .attr("height", 50)
-        .attr('stroke', 'red')
-        .attr("fill", "#ddd")
+//Fehler: wenn wipphebel dann gleiche farbklassen verwendet und somit hochregallager andere farbe. aber egal da wir andere seiten verwenden fpr einzelansichten
+function createSvgWipphebel(dataArray, Zeit) {
+    d3.xml("./media/img/Wipphebel.svg",
+        function (error, documentFragment) {
+            if (error) { console.log(error); return; }
+            var svgNode = documentFragment
+                .getElementsByTagName("svg")[0];
+            var main_chart_svg = d3.select("#Wipphebel")
+             if (document.getElementById('Wipphebel').getElementsByTagName('svg').length == 0) {
+                main_chart_svg.node().appendChild(svgNode);
+            }
+            svg = main_chart_svg.select("svg")
+            if (dataArray != "") {
+                svg.selectAll("rect.Wipphebel").remove();
+            }
+        });
 }
 
-function updateBearbeitungsstationMitBrennofen(svg, dataArray) {
-    svg.selectAll("rect.Brennofen").remove();
-    var colorForLichtschalterA = "back";
-    if (!dataArray[ArrayWithVariablesForBearbeitungsstationMitBrennofen[4]]) {
-        colorForLichtschalterA = "green";
-        console.log("FARBE HAT SICH GEÄNDET DAS IST GUT")
-    }
-    svg.append("rect")
-        .attr("class", "Hochregallager")
-        .attr("x", 500)
-        .attr("y", 500)
-        .attr("width", 100)
-        .attr("height", 100)
-        .attr('stroke', colorForLichtschalterA)
-        .attr("fill", colorForLichtschalterA)
-}
+// function createSvgBrennofen(dataArray, Zeit) {
+//     d3.xml("./media/img/Wipphebel.svg",
+//         function (error, documentFragment) {
+//             if (error) { console.log(error); return; }
 
-function updateStationen(svg, dataArray) {
-    updateHochregallager(svg, dataArray);
-    updateBearbeitungsstationMitBrennofen(svg, dataArray);
+//             var svgNode = documentFragment
+//                 .getElementsByTagName("svg")[0];
+//             var main_chart_svg = d3.select("#Wipphebel")
+//             // if (document.getElementById("div"))
+//             if (document.getElementById('Wipphebel').getElementsByTagName('svg').length == 0) {
+//                 main_chart_svg.node().appendChild(svgNode);
+//             }
+//             svg = main_chart_svg.select("svg")
+//             if (dataArray != "") {
+//                 svg.selectAll("rect.Wipphebel").remove();
+//             }
+//         });
+// }
+
+function updateStationen(dataArray, Zeit) {
+    createSvgUebersicht(dataArray, Zeit);
+    createSvgHochregallager(dataArray, Zeit);
+    //createSvgWipphebel(dataArray, Zeit)
+    //createSvgBrennofen(dataArray, Zeit);
 }
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
