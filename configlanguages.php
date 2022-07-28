40dcfff46040d9115046cb921a1892b48834b236
InnovationBuddy <uf6qr4w0r@mozmail.com>
2022-07-28 15:13:31 +0200

    Sprachenanpassung

--- configlanguages.php
+++ configlanguages.php
@@ -1,12 +1,13 @@
 <?php
 
+$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+
 if (!isset($_SESSION['lang'])) {
-    $_SESSION['lang'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    if ($_GET['lang'] == "de") {
+    if ($lang == "de") {
         $_SESSION['lang'] = "de";
-    } elseif ($_GET['lang'] == "en") {
+    } elseif ($lang == "en") {
         $_SESSION['lang'] = "en";
-    } elseif ($_GET['lang'] == "zh-CN") {
+    } elseif ($lang == "zh-CN") {
         $_SESSION['lang'] = "zh-CN";
     } else {
         $_SESSION['lang'] = "en";
@@ -21,8 +22,5 @@ if (!isset($_SESSION['lang'])) {
     else {
         $_SESSION['lang'] = "en";
     }
-} else {
-    $_SESSION['lang'] = "en";
 };
-
 include "language/" . $_SESSION['lang'] . ".php";
